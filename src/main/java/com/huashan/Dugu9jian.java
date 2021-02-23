bc425176649fc2f08520d3d1198205b6836911b4
linghuchong777888 <linghuchong777888@126.com>
2021-02-24 02:50:43 +0800

    独孤九剑心法

---
+++ src/main/java/com/huashan/Dugu9jian.java
@@ -0,0 +1,9 @@
+package com.huashan;
+
+public class Dugu9jian {
+    public static void main(String[] args){
+        System.out.println("dugu 9 jian hao !!");
+        System.out.println("dugu 9 jian hao !!");
+        System.out.println("dugu 9 jian hao !!");
+    }
+}
